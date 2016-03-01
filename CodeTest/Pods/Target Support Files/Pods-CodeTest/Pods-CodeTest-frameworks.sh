4b9d5f6ebe44dd4ff2c26a7e8130b7b162109068
Michael McKinney <mmckinney277@gmail.com>
2016-02-29 21:37:21 -0500

    Dropped JSONModel in favor of Argo and Curry. We will see how useable these are...

--- CodeTest/Pods/Target Support Files/Pods-CodeTest/Pods-CodeTest-frameworks.sh
+++ CodeTest/Pods/Target Support Files/Pods-CodeTest/Pods-CodeTest-frameworks.sh
@@ -84,12 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-CodeTest/JSONModel.framework"
-  install_framework "Pods-CodeTest/SWXMLHash.framework"
+  install_framework "Pods-CodeTest/Argo.framework"
+  install_framework "Pods-CodeTest/Curry.framework"
   install_framework "Pods-CodeTest/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-CodeTest/JSONModel.framework"
-  install_framework "Pods-CodeTest/SWXMLHash.framework"
+  install_framework "Pods-CodeTest/Argo.framework"
+  install_framework "Pods-CodeTest/Curry.framework"
   install_framework "Pods-CodeTest/SnapKit.framework"
 fi
